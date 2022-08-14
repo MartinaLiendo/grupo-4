131980119669dabd940e5a4ca825ca2fe4f4ad7b
Pablo Echegaray <paymar0799@gmail.com>
2022-08-14 00:07:55 -0300

    Trabajo Final Semestral

---
+++ Ejercicio9-ActualizarDireccion.sql
@@ -0,0 +1,5 @@
+use peluqueriacanina;
+
+UPDATE Dueno
+SET Direccion='Libertad 123'
+WHERE DNI=26585105;
---
+++ ejercicio9.py
@@ -0,0 +1,7 @@
+Historial5=(8520, 9510, 7530, 3570, 1590)
+
+def valorMayor():
+    print(max(Historial5))
+    return
+
+valorMayor()
