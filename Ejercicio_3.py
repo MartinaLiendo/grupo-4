8ee1cb6352798b802b5a87f58d5befa5ad50bf2f
JuanSmith620 <108648402+JuanSmith620@users.noreply.github.com>
2022-08-14 23:50:58 -0300

    Add files via upload
    Ejercicio N°3 de Python-Smith Juan

---
+++ Ejercicio_3.py
@@ -0,0 +1,8 @@
+#Ejercicio N°3
+#Genero la lista y la muestro
+Perro=[13,"Puppy","12/12/2012","Macho",123]
+print(Perro)
+#reemplazo los elementos y los muestros
+Perro[2]="13/12/2012"
+Perro[4]=28957346
+print(Perro)
